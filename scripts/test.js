a5dace448723b15ad8c0c6c89110c7421727c4e1
Li Hau Tan <tanlh@seagroup.com>
2020-10-02 10:42:01 +0800

    handle errors

--- scripts/test.js
+++ scripts/test.js
@@ -17,7 +17,8 @@ if (fixtures.find((fixture) => fixture.endsWith('.solo'))) {
 (async () => {
   for (const fixture of fixtures) {
     const entryFile = path.join(fixturePath, fixture, 'code/main.js');
-    const expected = require(path.join(fixturePath, fixture, 'expected.js'));
+    const expectedFile = path.join(fixturePath, fixture, 'expected.js');
+    const errorFile = path.join(fixturePath, fixture, 'error.js');
     const [title] = fs
       .readFileSync(path.join(fixturePath, fixture, 'README.md'), 'utf-8')
       .split('\n');
@@ -34,19 +35,34 @@ if (fixtures.find((fixture) => fixture.endsWith('.solo'))) {
 
     console.log(chalk.blue.underline(title));
     if (error !== undefined) {
-      console.log(chalk.red('Encounter error:'));
-      console.log(error);
+      if (!fs.existsSync(errorFile)) {
+        console.log(chalk.red('Encounter unexpected error:'));
+        console.log(error);
+      } else {
+        try {
+          const expectedError = require(errorFile);
+          expectedError(error);
+          console.log('😘 ' + chalk.green('Passed all assertions') + ' 🎉');
+        } catch (error) {
+          if (error.code === 'ERR_ASSERTION') {
+            console.log('😢 ' + chalk.red('Failed test:'));
+            printAssertionError(error);
+          } else {
+            throw error;
+          }
+        }
+      }
     } else if (result === undefined) {
       console.log(chalk.red('Not implemented'));
     } else {
       try {
+        const expected = require(expectedFile);
         expected(result, { base: path.join(fixturePath, fixture, 'code') });
-        console.log(chalk.green('Passed all assertions'));
+        console.log('😘 ' + chalk.green('Passed all assertions') + ' 🎉');
       } catch (error) {
         if (error.code === 'ERR_ASSERTION') {
-          console.log(chalk.red('Failed test:'));
-          console.log('-', chalk.underline(error.message));
-          console.log(chalk.blue('Expected:'), error.expected, ',', chalk.blue('got:'), error.actual);
+          console.log('😢 ' + chalk.red('Failed test:'));
+          printAssertionError(error);
         } else {
           throw error;
         }
@@ -55,3 +71,19 @@ if (fixtures.find((fixture) => fixture.endsWith('.solo'))) {
     console.log();
   }
 })();
+
+function printAssertionError(error) {
+  console.log('-', chalk.underline(error.message));
+  const operator =
+    error.operator === '=='
+      ? 'equal'
+      : error.operator === 'match'
+      ? 'match'
+      : '';
+  console.log(chalk.blue(`Expecting to ${chalk.underline.bold(operator)}`), error.expected, chalk.blue(','));
+  console.log(
+    chalk.blue('Received'),
+    chalk.green(JSON.stringify(error.actual)),
+    chalk.blue('instead.')
+  );
+}
